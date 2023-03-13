ab031020fd4ef7967b76e7e3edc0df9ef3a07e7f
Wildan Ahmadi Pohan <wildanahmadipohan@gmail.com>
2023-03-13 20:21:58 +0700

    Repair href link for github pages on IconText

--- src/parts/IconText.js
+++ src/parts/IconText.js
@@ -5,7 +5,7 @@ import wapLogo from '../assets/images/wap-logo.png';
 
 const IconText = () => {
   return (
-    <Button className='brand-icon' href='/' type='link'>
+    <Button className='brand-icon' href='/wapsite' type='link'>
       <img src={wapLogo} alt="WAP" />
     </Button>
   );
